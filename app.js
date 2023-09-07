28704b3f113c303aa445f97c78dc83794c8bf80b
Ahsankhan556 <ak5589667@gmail.com>
2023-09-07 09:06:22 +0845

    asimint javascript

---
+++ app.js
@@ -0,0 +1,145 @@
+
+                                                                     // ! QNO1
+
+// var num1 = 10;
+// var num2 =5;
+// var addition = num1 + num2;
+// document.getElementById("answer").textcontent = "the sum of your Result is :" +addition; 
+                                                                  // ! Qno 2 
+
+                                                                    //  ?part 1
+
+                                                               //  ?  substraction                                     
+// var num1= 3
+// var num2=5
+// alert( num1 - num2)
+                                                               //  !  Qno 2 
+
+                                                              //? (part#2 {multiplication})
+//  var num1=3
+//  var num2=5
+//  alert(num1 * num2)   
+                                                             // ! Qno 2
+
+                                                            // ? (part#3 {division})              
+// var num1=3
+// var num2=5
+// alert(num1 / num2)               
+                              // Qno2 (part #4{modulus})  
+ // var num1=3
+// var num2=5
+// alert(num1 % num2)                        
+                                                            //? Qno3(parta.b)
+// var num1 = 2
+// alert( --num1)                   
+ 
+                                                            // ?Qno 3 (part c)
+// var num1 = 30
+// var num2 = 20
+// alert( num1 + num2)                  
+                                                            // ? Qno 3 (part d,e )
+// var num1 = 5
+// alert( ++ num1)                        
+                                                             //? Qno 3 (part f,g)
+// var num1 = 6
+// var num2 = 7
+// alert(num1 + num2)                      
+                                                             // ?Qno 3 (part i,j)
+// var num1 = 13
+// alert(--num1 )
+                                                            //?Q no 3  (part k)
+// var num1 = 6
+// var num2 = 3
+// alert(num1/num2)
+                       
+
+
+                                                             //! Q no3 
+                                                            //  ?(part l )
+// var num1 = 6
+// var num2 = 3
+// alert(num1%num2)
+
+                                                             //!Qno 4 
+
+                                                            //? (part ;t )
+// var ticketprice =  600
+// var beyingprice = 5
+// alert(ticketprice*beyingprice)
+                                                            //!Qno 6
+
+                                                           // ?part (A)
+
+// var celsiustemp = 25;
+                                    // p?art( B )
+// var fahrenhetTemp = (celsiustemp * 9 / 5) + 32;
+// document.write(celsiustemp + fahrenhetTemp + fahrenhetTemp + "°f" + "<br>");
+
+                                                          // ? part (c)
+
+// var faharenheiTemp2 = 77;
+
+                                                          //?part (d)
+
+// var celslustemp2 =(faharenheiTemp2 - 32 ) * 5 / 9;
+// document.write(faharenheiTemp2 + "°f is" + celsiustemp + "°c");
+
+                                                           //  !Qno 7
+
+// var item1 = 650
+// var quantity1 = 3
+// var item2 = 100
+// var quantity2 = 7
+
+// var total = (item1 * quantity1) + (item2 * quantity2)
+
+// document.write("price of item 1 is " + item1 + "<br>")
+// document.write("Quantity of item is  " + quantity1 + "<br>")
+// document.write("price of item 2 is " + item2 + "<br>")
+// document.write("Quantity of item 2 is " + quantity2 + "<br>")
+// document.write("Total cost of your order is " + total + "<br>")
+
+  // 
+
+                                                        //  ! Q NO 8
+
+
+// var totalMarkes = 980
+// var markObtained = 804
+// document.write("Total Marks: 980" + "<br>")
+// document.write("Marks obtained: 804" + "<br>")
+
+// document.write( "Percentage:"+ markObtained *100/totalMarkes)
+
+
+                                                      // !Qno 9
+
+// var  usDollar1 = 104
+// var usDollar10 = 10
+// document.write( usDollar1 *usDollar10 +"<br>")
+
+// var  SaudiRiyals1 = 28
+// var   SaudiRiyals25 = 25
+// document.write( SaudiRiyals1*SaudiRiyals25 +"<br>")
+// var finalResult = Number( usDollar1 *usDollar10 ) + Number( SaudiRiyals1*SaudiRiyals25)
+// document.write("total currency in PKR: "  + finalResult)
+
+
+                                                      //  ! Q no 10
+
+// var num = 10
+// var Result = (num + 5) * 10 / 2
+// document.write("The total result is : " + Result) 
+
+
+                                                        // ! Q no 11
+
+// var current = 2023
+// var birthday = 2006
+// var age = current - birthday
+
+// document.write("current year is = " + current + "<br>")
+// document.write("birthday year is = " + birthday + "<br>")
+// document.write("your age year is = " + age + "<br>")
+
+                                                      // ! Q no 12
